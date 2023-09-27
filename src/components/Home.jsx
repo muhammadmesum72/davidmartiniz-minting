e9cd8e3a78a158a11fd82e578408e0751cc0121e
muhammadmesum72 <me.m.mesum@gmail.com>
2023-09-27 22:37:45 +0500

    Icons size change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,13 +22,13 @@ const Home = () => {
 
             <div className="absolute top-[25%] md:top-[25%] w-2/3 md:w-1/2 mx-auto left-0 right-0 flex items-center justify-around px-12">
               <a href="https://docs.communiteez.xyz" target="_blank">
-                <img src={WhitePaperIcon} alt="" className="w-9 md:w-20" />
+                <img src={WhitePaperIcon} alt="" className="w-7 md:w-20" />
               </a>
               <a href="https://twitter.com/communiteez" target="_blank">
-                <AiOutlineTwitter className=" text-2xl md:text-6xl cursor-pointer text-blue-500 " />
+                <AiOutlineTwitter className=" text-xl md:text-6xl cursor-pointer text-blue-500 " />
               </a>
               <a href="#" target="_blank">
-                <FaDiscord className=" text-2xl md:text-6xl cursor-pointer text-indigo-600 " />
+                <FaDiscord className=" text-xl md:text-6xl cursor-pointer text-indigo-600 " />
               </a>
             </div>
           </div>
