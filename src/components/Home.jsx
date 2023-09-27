65abc962a3b8d88c004045b8b568fb0076407d15
muhammadmesum72 <me.m.mesum@gmail.com>
2023-09-27 21:28:27 +0500

    version 1.3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import LightRoom from "../assets/light room.jpg";
 import DarkRoom from "../assets/dark room.jpg";
 import { AiOutlineTwitter } from "react-icons/ai";
+import { FaDiscord } from "react-icons/fa";
 import MoonImg from "../assets/moon.png";
 import DayImg from "../assets/day.png";
 import WhitePaperIcon from "../assets/white-paper.png";
@@ -16,13 +17,16 @@ const Home = () => {
     <>
       <div className="w-full">
         {isLightSwitch ? (
-          <div>
-            <div className="absolute top-0 w-full flex items-center justify-around mt-12">
+          <div className="">
+            <div className="absolute top-28 md:top-48 md:w-1/2 mx-auto left-0 right-0 flex items-center justify-around px-12">
               <a href="https://docs.communiteez.xyz" target="_blank">
-                <img src={WhitePaperIcon} alt="" className="w-16" />
+                <img src={WhitePaperIcon} alt="" className="w-16 md:w-20" />
               </a>
               <a href="https://twitter.com/communiteez" target="_blank">
-                <AiOutlineTwitter className="text-5xl cursor-pointer text-blue-500 " />
+                <AiOutlineTwitter className=" text-5xl md:text-6xl cursor-pointer text-blue-500 " />
+              </a>
+              <a href="#" target="_blank">
+                <FaDiscord className=" text-5xl md:text-6xl cursor-pointer text-indigo-600 " />
               </a>
             </div>
             <img src={LightRoom} alt="" className="h-full w-full duration-500 ease-in-out" />
@@ -40,15 +44,15 @@ const Home = () => {
       >
         {/* <img src={} alt="" /> */}
         <div
-          className={` w-24 h-12 flex items-center  rounded-full p-1 cursor-pointer ${
+          className={` w-32 h-16 flex items-center  rounded-full p-2 cursor-pointer ${
             isLightSwitch ? "bg-gray-700" : "bg-gray-700"
           }`}
         >
           {/* switch  */}
 
           <div
-            className={`bg-white w-9 h-9 rounded-full shadow-md transform duration-300 ease-in-out ${
-              isLightSwitch ? "transform translate-x-12 bg-transparent" : null
+            className={`bg-white w-12 h-12 rounded-full shadow-md transform duration-300 ease-in-out ${
+              isLightSwitch ? "transform translate-x-16 bg-transparent" : null
             }`}
           >
             {isLightSwitch ? (
